5d5cb0968d525dfd7f059f4d6301b20b5cda2512
Dimuthu-abethunge <56988418+Dimuthu-abethunge@users.noreply.github.com>
2021-07-18 13:52:21 +0530

    Conductor Login Server

--- server/routes/smartride.js
+++ server/routes/smartride.js
@@ -44,4 +44,53 @@ router.post("/register", validInfo, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+//login------------------------------------------------------------
+
+router.post("/login", validInfo, async (req, res) => {
+  try {
+    //1. destructure the req.body
+    const { email, password } = req.body;
+
+    //2. check if user dosen't exist(if not then thrwo error)
+
+    const user = await pool.query("SELECT * FROM users WHERE user_email= $1 AND is_admin='0'", [
+      email
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("Email or Password is incorrect");
+    }
+
+    //3. check if incoming password is the same the db password
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+
+    if (!validPassword) {
+      return res.status(401).json("Email or Password is incorrect");
+    }
+
+    //4. give them the jwt token
+
+    const token = jwtGenerator(user.rows[0].user_id);
+
+    res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server Error");
+  }
+});
+
+router.get("/is-verify", authorization, async (req, res) => {
+  try {
+    res.json(true);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error.");
+  }
+});
+
 module.exports=router;

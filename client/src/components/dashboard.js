1135c455b185e3acfefbc9cc96d5b7d5a910f602
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2021-08-19 12:29:44 +0530

    conductor location update first commit

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,25 +1,68 @@
 import { Button, Grid } from '@material-ui/core';
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import './style/dashboard.css';
 import Header from './widget/header';
 import SideNav from './widget/sidenav';
+import useGeoLocation from './widget/getCurLocation';
 
 const Dashboard = ({ setAuth }) => {
 
+    const[ newLat, setNewLat] = useState([]);
+    const[ newLng, setNewLng] = useState([]);
+
+    var id = localStorage.getItem('user_id');
+    console.log(id);
+
+    const location = useGeoLocation();
+    //console.log(JSON.stringify(location));
+    var latitude = JSON.stringify(location.coordinates['lat']);
+    var longitude = JSON.stringify(location.coordinates['lng']);
+    console.log(latitude);
+    console.log(longitude);
+
+   
+
+    async function updateBusLocation(latitude, longitude) {
+        const body = {latitude, longitude};
+        console.log(latitude);
+        console.log(longitude);
+        console.log(id);
+
+        const updateDet = await fetch(`http://localhost:5001/smartride/update/${id}`, {
+                method: "PUT",
+                headers: {"Content-Type" : "application/json"},
+                body: JSON.stringify(body)
+        })
+
+        const parseRes = await updateDet.json();
+        console.log(parseRes);
+
+        // if(parseRes) {
+        //     window.location.reload();
+        // }
+        // else {
+        //     console.log(parseRes);
+        // }
+    }
+
+    useEffect(() => {
+        //setTimeout( updateBusLocation(), 5000);
+        updateBusLocation();
+    },[]);
+    
     return(
         <Fragment>
             <div className="body">
-                {/* <Grid > */}
+               
                 <Header />
-                {/* </Grid > */}
+                <SideNav />
                 
-                {/* <Grid > */}
-                    <SideNav />
-                {/* </Grid > */}
-
-                <Button 
-                    
-                />
+                {/* <div>
+                {location.loaded
+                    ? JSON.stringify(location)
+                    : "Location data not available yet."}
+                </div> */}
+                
             </div>
         </Fragment>
     );
--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app =  express();
 const cors = require("cors");
 
-
+ 
 app.use(express.json());
-app.use(cors());
+app.use(cors()); 
 
 //register and login
 app.use("/smartride", require("./routes/smartride"));
 
-app.listen(5001, () => { 
+app.listen(5001, () => {  
     console.log("server is running on port 5001");
-});
+}); 

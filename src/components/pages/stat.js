d74a37d101fc0a0799cd479dd03a275668d80dc6
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2021-09-20 18:58:02 +0530

    add paid list

--- src/components/pages/stat.js
+++ src/components/pages/stat.js
@@ -2,11 +2,19 @@ import React, {Fragment, useState, useEffect} from 'react';
 import { toast } from 'react-toastify';
 import {Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles,withStyles } from '@material-ui/core/styles';
 import Header from '../widget/header';
 import SideNav from '../widget/sidenav';
 import '../style/stat.css';
 import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 
 const useStyles = makeStyles((theme) => ({
     
@@ -14,19 +22,48 @@ const useStyles = makeStyles((theme) => ({
       width: '60px',
       height: '50px',
     },
+
+    table: {
+        maxWidth: 600,
+      },
+
+      cell: {
+          width: 150,
+      }
 }));
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
 
 const Stat = () => {
 
     const classes = useStyles();
 
     var id = localStorage.getItem('user_id');
+    var bus_id = localStorage.getItem('bus_id');
+    console.log(bus_id);
 
     const [passengerCount, setPassengerCount] = useState([]);
     const [todayIncome, seTodayIncome] = useState([]);
     const [conName, setConName] = useState([]);
     const [busName, setBusName] = useState([]);
     const [prevIncome, setPrevIncome] = useState([]);
+    const [paidList, setPaidList] = useState([]);
  
     async function getTodayPassengerCount() {
         const res = await fetch("http://localhost:5000/conductor/stat");
@@ -115,6 +152,19 @@ const Stat = () => {
         //console.log(bus);
     }
 
+    const body1 = {bus_id};
+    async function getConductName() {
+        const res = await fetch("http://localhost:5000/conductor/stat/paidlist", {
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(body1)
+        });
+
+        const list = await res.json();
+
+        setPaidList(list);
+    }
+
     useEffect(() => {
         getTodayPassengerCount();
         getTodayIncome();
@@ -154,11 +204,37 @@ const Stat = () => {
                                 </center>
                             </div>
                         </div>
-                        <div className="form-bottom">
+                        {/* <div className="form-bottom">
                             <h1>{conName}</h1>
                             <h1>{busName}</h1>
-                        </div>
+                        </div> */}
                     </center>
+                </div>
+
+                <div className="list_conductor_container">
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="customized table">
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell className={classes.cell} align="center">Name</StyledTableCell>
+                                    <StyledTableCell className={classes.cell} align="center">Phone No</StyledTableCell>
+                                    <StyledTableCell className={classes.cell} align="center">Email</StyledTableCell>
+                                    <StyledTableCell className={classes.cell} align="center">Cost</StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+
+                            <TableBody>
+                                {paidList.map((row) => (
+                                    <StyledTableRow key={row.ride_id}>
+                                        <StyledTableCell className={classes.cell} align="center" component="th" scope="row">{row.uname}</StyledTableCell>
+                                        <StyledTableCell className={classes.cell} align="center">{row.phone_number}</StyledTableCell>
+                                        <StyledTableCell className={classes.cell} align="center">{row.email}</StyledTableCell>
+                                        <StyledTableCell className={classes.cell} align="center">{row.cost}</StyledTableCell>                
+                                    </StyledTableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </div>
                 
                
